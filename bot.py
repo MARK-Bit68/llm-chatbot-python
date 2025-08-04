cf806f71c312e967e6b902f9e9842da8ec029bdc
kosar <kosar.jaff@gmail.com>
2025-08-03 17:06:45 -0700

    Remove direct embeddings import from bot.py to prevent st.secrets error

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ st.set_page_config("FMCG RAG Chatbot", page_icon=":chart_with_upwards_trend:")
 from utils import write_message
 from solutions.agent import generate_response
 from graph import graph
-from llm import embeddings
 
 # Set up Session State
 if "messages" not in st.session_state:
